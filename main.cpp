3bf8eb0ac309c65f8262d08daf968c6b7d329d61
HicaroD <hdanrlley1@gmail.com>
2024-07-27 16:20:40 -0300

    removing comments and include directive

--- main.cpp
+++ main.cpp
@@ -4,14 +4,12 @@
 #include <llvm/IR/Verifier.h>
 #include <llvm/Support/raw_ostream.h>
 
-#include <vector>
 
 int main() {
   llvm::LLVMContext context;
   llvm::Module *module = new llvm::Module("hello", context);
   llvm::IRBuilder<> builder(context);
 
-  // Create main function: int main() { ... }
   llvm::FunctionType *main_type =
       llvm::FunctionType::get(builder.getInt32Ty(), false);
   llvm::Function *main_function = llvm::Function::Create(
@@ -20,14 +18,13 @@ int main() {
       llvm::BasicBlock::Create(context, "entry", main_function);
   builder.SetInsertPoint(entry_bb);
 
-  // Get printf function declaration
   std::vector<llvm::Type *> printf_args;
   printf_args.push_back(builder.getInt8PtrTy());
   llvm::FunctionType *printf_type =
       llvm::FunctionType::get(builder.getInt32Ty(), printf_args, true);
-  llvm::FunctionCallee printf = module->getOrInsertFunction("printf", printf_type);
+  llvm::FunctionCallee printf =
+      module->getOrInsertFunction("printf", printf_type);
 
-  // Create format string
   llvm::Value *format_str = builder.CreateGlobalStringPtr("Hello, World!\n");
 
   // Call printf
